c750dbf6c259b59c05dc2359d640b926baed2751
alefCS50 <allan.efendic@icloud.com>
2023-06-05 13:05:20 +0200

    feat: employer page

---
+++ src/firebase/admin.ts
@@ -0,0 +1,15 @@
+import * as admin from 'firebase-admin';
+
+/* eslint-disable import/prefer-default-export */
+import { credential } from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+
+const serviceAccount = require('./ServiceAccountFile.json');
+
+if (!admin.apps.length) {
+ initializeApp({
+   credential: credential.cert(serviceAccount),
+ });
+}
+
+export const adminSDK = admin;
