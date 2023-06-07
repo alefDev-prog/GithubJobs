e66aa70a9fbb9be76697d0b59e704b1deefb4e31
alefCS50 <allan.efendic@icloud.com>
2023-06-07 16:16:16 +0200

    feat: working login and accesstoken store

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,11 +1,15 @@
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/config";
-import { useState } from "react";
+import { use, useState } from "react";
+
+
 
 export const useLogin = () => {
   const [error, setError] = useState<null | string>(null);
   const [isPending, setIsPending] = useState(false);
+  const [accessToken, setAccessToken] = useState<string | undefined>("");
   const provider = new GithubAuthProvider();
+  
 
   const login = async () => {
     setError(null);
@@ -16,9 +20,8 @@ export const useLogin = () => {
       if (!res) {
         throw new Error("Could not complete signup");
       }
-
-      const user = res.user;
-      console.log(user);
+      setAccessToken(GithubAuthProvider.credentialFromResult(res)?.accessToken)
+      
       setIsPending(false)
     } catch (error) {
       console.log(error);
@@ -29,5 +32,5 @@ export const useLogin = () => {
     }
   };
 
-  return { login, error, isPending };
+  return { login, error, isPending, accessToken };
 };
