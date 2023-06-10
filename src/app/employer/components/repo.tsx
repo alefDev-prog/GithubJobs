5e3133784760dc1244f49209b607fa520f044238
alefCS50 <allan.efendic@icloud.com>
2023-06-10 14:02:53 +0200

    fix: migrated employer to reducer

--- src/app/employer/components/repo.tsx
+++ src/app/employer/components/repo.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch} from "react";
+import { Action, ActionKinds } from "./reducer";
 
-export default function Repo({repository, setCurrentRepo}:{repository:any, setCurrentRepo: Dispatch<SetStateAction<object>>}) {
+export default function Repo({repository, setCurrentRepo}:{repository:any, setCurrentRepo: Dispatch<Action>}) {
     function handleClick() {
-        setCurrentRepo(repository);
+        setCurrentRepo({type: ActionKinds.SET_CURRENT_REPO, payload: repository});
     }
     return (
         <div className="list-group-item list-group-item-action" onClick={handleClick}>{repository.name}</div>
