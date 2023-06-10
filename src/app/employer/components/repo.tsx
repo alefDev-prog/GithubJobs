5014b4190131ed590ac38ec4ee63964832db4e1d
alefCS50 <allan.efendic@icloud.com>
2023-06-10 13:48:24 +0200

    feat: basic form

--- src/app/employer/components/repo.tsx
+++ src/app/employer/components/repo.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-export default function Repo({repository}:{repository:any}) {
-    console.log(repository);
+import { Dispatch, SetStateAction } from "react";
+
+export default function Repo({repository, setCurrentRepo}:{repository:any, setCurrentRepo: Dispatch<SetStateAction<object>>}) {
+    function handleClick() {
+        setCurrentRepo(repository);
+    }
     return (
-        <div className="list-group-item list-group-item-action">{repository.name}</div>
+        <div className="list-group-item list-group-item-action" onClick={handleClick}>{repository.name}</div>
     )
 }
--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -8,6 +8,7 @@ import Repo from "./components/repo";
 export default function Employer() {
     const currentUser = useAuth();
     const [repoInfo, setRepoInfo] = useState<any[]>([]);
+    const [currentRepo, setCurrentRepo] = useState<object>({});
 
     useEffect(() => {
         console.log(currentUser?.token);
@@ -28,26 +29,60 @@ export default function Employer() {
     return (
         <main className="container-xl">
             <div className="row">
+
+                {/* The selection of repo */}
                 <div className="col">
                     <div className="card border-1">
                         <div className="card-header">
                             <h3 className="card-title">Your repositories</h3>
                         </div>
-                        <button onClick={() => console.log(currentUser)}>Check</button>
+                        <button onClick={() => console.log(currentRepo)}>check</button>
                         <div className="list-group" style={{maxHeight: "300px", overflowY:"scroll"}}>
 
                             {repoInfo.map((obj, index): React.ReactNode => {
-                                return <Repo repository={obj} key={index} />
+                                return <Repo repository={obj} setCurrentRepo={setCurrentRepo} key={index}/>
                             })}
                         </div>
                     </div>
                 </div>
+
+                {/* Adding description of job for repo */}
                 <div className="col">
-                    ok
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="jobTitle">Job Title</label>
+                            <input type="text" className="form-control form-control-lg" id="jobTitle" placeholder="Enter job title" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="jobDescription">Job Description</label>
+                            <textarea className="form-control" id="jobDescription" rows={3}></textarea>
+                        </div>
+                        <div className="form-group">
+                            <label>Payment</label>
+                            <select className="form-control">
+                                <option>Fixed price</option>
+                                <option>Per milestone</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="jobPeriod">Job Period</label>
+                            <input type="text" className="form-control" id="jobPeriod" placeholder="Enter job period" />
+                        </div>
+                        <div className="form-group col-auto">
+                            <label htmlFor="jobSalary">Job Salary</label>
+                            <div className="input-group mb-2">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-text">$</div>
+                                </div>
+                                <input type="number" className="form-control" id="jobSalary" placeholder="Enter job salary" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
                 </div>
-                
             </div>
-        
         </main>
         
     );
