c1547ddb27e07cc46c0c055b75aae67fc3420cab
alefCS50 <allan.efendic@icloud.com>
2023-06-29 17:15:59 +0200

    feat: added application only to applicant in db

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,19 +1,23 @@
 "use client"
 
+import { useAuth } from "@/context/AuthContext";
 import { db } from "@/firebase/config";
 import { where } from "@firebase/firestore";
-import { DocumentData, collectionGroup, getDocs, query } from "firebase/firestore";
+import { DocumentData, collection, collectionGroup, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function Job() {
 
-    const search = useSearchParams();
-   
+  const search = useSearchParams();
+  const id = search.get("id");
+  const currentUser = useAuth()
+  
 
-    const [currentJob, setCurrentJob] = useState<DocumentData|null>(null);
-    const id = search.get("id")
+  const [currentJob, setCurrentJob] = useState<DocumentData|null>(null);
+  const letter = useRef<HTMLTextAreaElement | null>(null);
+    
 
   useEffect(() => {
 
@@ -29,34 +33,57 @@ export default function Job() {
       } catch(error) {
         console.log(error);
       }
-      
-
     
     };
 
     fetchJobDocument();
   }, []);
 
+
+
+  async function handleSubmit() {
+    const userId = currentUser?.user?.uid;
+
+    if(userId){
+        const docRef = doc(db, "users", userId);
+        const subCollectionRef = collection(docRef, "userApplications");
+        const jobDoc = doc(subCollectionRef);
+        
+
+        const applicationInfo = {
+          jobId: currentJob?.id,
+          applicant: {
+            name: currentUser.user?.displayName,
+            image: currentUser.user?.photoURL,
+            id: currentUser.user?.uid,
+          },
+          coverletter: letter.current?.value,
+        }
+
+        await setDoc(jobDoc, applicationInfo)
+    }
+  }
+
   
-    return (
-      <div className="container mt-4">
-      {currentJob ? (
-        <div className="card p-4">
-          <h1 className="display-4">{currentJob.title}</h1>
-          <p className="lead">{currentJob.description}</p>
-          <ul className="list-unstyled">
-            <li>Period: {currentJob.period}</li>
-            <li>Payment: {currentJob.payment}</li>
-            <li>Salary: ${currentJob.salary}</li>
-          </ul>
-          <textarea className="form-control mb-3" rows={5} placeholder="Write your cover letter"></textarea>
-          <button className="btn btn-success" style={{ backgroundColor: "#14A100" }}>Submit</button>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-    )
+  return (
+    <div className="container mt-4">
+    {currentJob ? (
+      <div className="card p-4 bg-light">
+        <h1 className="display-4">{currentJob.title}</h1>
+        <p className="lead">{currentJob.description}</p>
+        <ul className="list-unstyled">
+          <li>Period: {currentJob.period}</li>
+          <li>Payment: {currentJob.payment}</li>
+          <li>Salary: ${currentJob.salary}</li>
+        </ul>
+        <textarea className="form-control mb-3" rows={5} placeholder="Write your cover letter" ref={letter}></textarea>
+        <button onClick={handleSubmit} className="btn btn-success" style={{ backgroundColor: "#14A100" }}>Submit</button>
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
+  )
 
 
 }
