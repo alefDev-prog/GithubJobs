74c1211ffad683883f4390b6362987af6ab21bee
alefCS50 <allan.efendic@icloud.com>
2023-07-17 16:20:44 +0200

    feat: working chatting

---
+++ src/app/chat/components/input.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { db } from "@/firebase/config";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { FormEvent } from "react";
+
+export default function Input({chatId, currentId}:{chatId:string|null, currentId: string|undefined}) {
+
+
+
+    function submit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const message = (form.elements[0] as HTMLInputElement).value;
+        if(chatId && currentId) {
+            updateDoc(doc(db, "chats", chatId), {
+                messages: arrayUnion({currentId, message})
+            });
+        }
+
+    }
+
+
+    return (
+        <div className="d-fixed bottom-0 w-100 bg-light p-3">
+            <form onSubmit={submit}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Enter message..." />
+                    <button className="btn btn-primary" type="submit" id="button-addon2">Send</button>
+                </div>
+            </form>
+        </div>
+    )
+}
