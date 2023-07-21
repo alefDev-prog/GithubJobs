683b02ef2569edd565a555e1f17c2de753016cbd
alefCS50 <allan.efendic@icloud.com>
2023-07-21 18:14:49 +0200

    feat: auth setup

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,24 +1,26 @@
 import { auth } from "firebase-admin";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  const sessionCookie = req.cookies.get('serverCookie');
-  const session = sessionCookie?.value;
-  //return NextResponse.json({message: sessionCookie});
 
-  if (!session) {
+export async function GET(req: NextRequest, res: NextResponse) {
+  const sessionCookie = req.cookies.get("serverCookie");
+
+  // Ensure sessionCookie and its value exists
+  if (!sessionCookie || !sessionCookie.value) {
     return NextResponse.json({isLogged: false}, {status: 401});
   }
-  else {
-    const decodedClaims = await auth().verifySessionCookie(session, true);
+  
+  // Use sessionCookie.value, which is a string, instead of the whole sessionCookie object
 
-    if (!decodedClaims) {
-      return NextResponse.json({isLogged: false}, {status: 401});
-    }
+  //The original cookie needs to be modified because it contains quotation marks which ruin the verification
+  const modifiedCookie = sessionCookie.value.substring(1, sessionCookie.value.length-1);
+
+
+  const {uid} = await auth().verifySessionCookie(modifiedCookie, true);
+  if (!uid) {
+    return NextResponse.json({isLogged: false}, {status: 401});
   }
 
-  
-
-  return NextResponse.json({isLogged: true}, {status: 200});
-  
+  return NextResponse.json({userId: uid}, {status: 200});
 }
+
