203dd70e8bceb809e173fd715957d34fd13e3a0a
alefDev-prog <allan.efendic@icloud.com>
2023-08-09 18:32:30 +0200

    feat: view currentJobs on profile page

--- src/app/components/profile/components/jobcard.tsx
+++ src/app/components/profile/components/jobcard.tsx
@@ -14,24 +14,32 @@ export default function JobCard({jobs}: {jobs: jobInfo[]}) {
         setItems(jobs);
     }
 
+   
+    if(items.length === 0) {
+        return (
+            <p className="mt-3">You have not published any jobs</p>
+        )
+    }
+
 
     return (
         <>
-            {items.map((job: jobInfo, index:number) => (
-                <div className="card mt-3 flex-grow-1" key={index}>
-                    <div className="card-body">
-                        <Link href={{pathname:"/job", query:{ id: job.id}}} className="link-underline-primary"><h5 className="card-title text-primary">{job.title}</h5></Link>
-                        {
-                        job.description.length > 50 
-                        ? <p className="card-text">Description: {job.description.substring(0,50)} . . .</p>
-                        : <p className="card-text">Description: {job.description}</p>
-                        }
-                        <p className="card-text">Payment: {job.payment}</p>
-                        <p className="card-text">Period: {job.period}</p>
-                    </div>
-            </div>
-            ))}
-            {items.length !== jobs.length && <p onClick={showMore} className="text-primary text-decoration-underline btn">Show all</p>}
+  {items.map((job: jobInfo, index:number) => (
+    <div className="card mt-3 flex-grow-1" key={index}>
+        <div className="card-body position-relative">
+        {job.assignee && <span className="position-absolute bottom-0 end-0 me-2 mb-2 text-primary">Assigned</span>}
+            <Link href={{pathname:"/job", query:{ id: job.id}}} className="link-underline-primary"><h5 className="card-title text-primary">{job.title}</h5></Link>
+            {
+            job.description.length > 50 
+            ? <p className="card-text">Description: {job.description.substring(0,50)} . . .</p>
+            : <p className="card-text">Description: {job.description}</p>
+            }
+            <p className="card-text">Payment: {job.payment}</p>
+            <p className="card-text">Period: {job.period}</p>
+        </div>
+    </div>
+))}
+{items.length !== jobs.length && <p onClick={showMore} className="text-primary text-decoration-underline btn">Show all</p>}
         </>
         
         
--- src/app/components/profile/profile.tsx
+++ src/app/components/profile/profile.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import JobCard from "./components/jobcard";
 import ApplicationCard from "./components/applicationcard";
+import CurrentJob from "./components/currentJob";
 
 
 export default async function Profile() {
@@ -17,39 +18,58 @@ export default async function Profile() {
         const userData = await getData(uid) as {
             userJobsData: jobInfo[],
             userApplicationsData: userApplication[],
+            currentJobsData: jobInfo[],
             githubData: githubData
         };
-        //console.log(userData.githubData.followers);
+        
         return (
-            <div className="container-fluid">
-    <div className="row justify-content-evenly">
-        <div className="col-lg-3 col-md-6 col-sm-12 mt-3 mb-3 ">
-            <div className="card flex-grow-1">
-                <div className="d-flex align-items-start">
-                    <Image src={userData.githubData.image} alt="Profile" width={200} height={200} />
+            <div className="container-fluid mt-3">
+     <div className="row justify-content-center gy-3">
+        <div className="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
+                  <div className="card">
+                    <div className="d-flex align-items-start">
+                      <Image src={userData.githubData.image} alt="Profile" width={200} height={200} />
+                    </div>
+                    <div className="card-body">
+                      <h3 className="card-title text-primary">{userData.githubData.name}</h3>
+                      <p className="card-text">Followers: {userData.githubData.followers}</p>
+                      <p className="card-text">Following: {userData.githubData.following}</p>
+                      <p className="card-text">Location: {userData.githubData.location}</p>
+                      <p className="card-text">Email: {userData.githubData.email}</p>
+                      <Link href={userData.githubData.url} className="btn btn-primary d-block text-white">Github Profile</Link>
+                      <Logout />
+                    </div>
+                  </div>
                 </div>
-                <div className="card-body">
-                    <h3 className="card-title text-primary">{userData.githubData.name}</h3>
-                    <p className="card-text">Followers: {userData.githubData.followers}</p>
-                    <p className="card-text">Following: {userData.githubData.following}</p>
-                    <p className="card-text">Location: {userData.githubData.location}</p>
-                    <p className="card-text">Email: {userData.githubData.email}</p>
-                    <Link href={userData.githubData.url} className="btn btn-primary d-block text-white">Github Profile</Link>
-                    <Logout />
+                 <div className="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h3 className="card-title">Current Jobs</h3>
+                      <CurrentJob jobs={userData.currentJobsData} />
+                    </div>
+                  </div>
                 </div>
+                 <div className="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h3 className="card-title">Jobs Published</h3>
+                      <JobCard jobs={userData.userJobsData} />
+                    </div>
+                  </div>
+                </div>
+                 <div className="col-12 col-sm-6 col-md-6 col-lg-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h3 className="card-title">Job Applications</h3>
+                      <ApplicationCard apps={userData.userApplicationsData} />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-        <div className="col-lg-5 col-md-6 col-sm-12 mt-3 mb-3 border bg-secondary rounded">
-            <h3>Jobs Published</h3>
-            <JobCard jobs={userData.userJobsData} />
-        </div>
-        <div className="col-lg-3 col-md-12 mt-3 mb-3 border bg-secondary rounded">
-            <h3>Job Applications</h3>
-            <ApplicationCard apps={userData.userApplicationsData} />
-        </div>
-    </div>
-</div>
-        )
+          );
+          
+          
     }
 
     else return (
