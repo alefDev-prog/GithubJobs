7d3467b6e188d25fd8e2ce680ea2636424fbce5e
alefDev-prog <allan.efendic@icloud.com>
2023-08-19 19:34:35 +0200

    feat: probably working fork api

--- src/app/api/fork/route.ts
+++ src/app/api/fork/route.ts
@@ -1,4 +1,6 @@
 import verifyAuth from "@/authMiddleware/auth";
+import { jobInfo } from "@/interfaces/interface";
+import { Octokit } from "@octokit/rest";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,6 +11,21 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const cookieStore = cookies()
     const accessToken = cookieStore.get('accessToken')?.value;
+    const octokit = new Octokit({ 
+        auth: accessToken
+    });
+    const job = await req.json() as jobInfo;
+    
+    try {
+        const url = new URL(job.repository.html_url);
+        const [ , owner, repo ] = url.pathname.split('/');
+        const response = await octokit.request('POST /repos/{owner}/{repo}/forks', {
+            owner,
+            repo,
+        })
+    } catch(error) {
+        return new Error("Could not fork");
+    }
 
 
     return NextResponse.json({accessToken})
