ff938899dbd2d8beaf97c2c0ff96021a0c54887d
alefDev-prog <allan.efendic@icloud.com>
2023-08-26 14:40:33 +0200

    feat: submitting work successfully

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -6,7 +6,20 @@ import loadMess from "./utils/loadMessages";
 export default async function Messages() {
     const messages = await loadMess();
     if(messages instanceof Error) return <h1>Error</h1>
-    if(!messages) return <h1></h1> 
+    if(!messages) return <h1></h1>
+    
+    
+    if(messages.length === 0) {
+        return (
+            <div className="container mt-3">
+            <h1 className="mb-4 text-primary">Messages</h1>
+            <div>
+                <h4>You have no messages</h4>
+            </div>
+            
+        </div>
+        )
+    }
     
     return (
         <div className="container mt-3">
