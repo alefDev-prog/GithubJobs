dc17ff299a329e73dc93cece92b39f33908a6758
alefDev-prog <allan.efendic@icloud.com>
2023-09-01 14:49:05 +0200

    feat: probably working request_changes

--- src/app/job/components/requestModal.tsx
+++ src/app/job/components/requestModal.tsx
@@ -2,15 +2,32 @@
 
 import { Dispatch } from "react";
 import { actionKind, jobControlAction, jobControlState} from "./reducer/reducer";
+import { jobInfo } from "@/interfaces/interface";
 
 
-export default function RequestModal({state, dispatch}: {state: jobControlState, dispatch: Dispatch<jobControlAction>}) {
+export default function RequestModal({state, dispatch, job}: {state: jobControlState, dispatch: Dispatch<jobControlAction>, job:jobInfo}) {
 
     
  
     function handleClose() {
         console.log("here");
         dispatch({type: actionKind.CLOSE_REQUEST_MODAL});
+    }
+
+
+    async function handleRequest() {
+        const requestAction = await fetch("/api/requestChange", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(job)
+        });
+        if(requestAction.ok) {
+            const resp = await requestAction.json();
+            console.log(resp);
+        }
+
     }
 
     return (
@@ -26,11 +43,11 @@ export default function RequestModal({state, dispatch}: {state: jobControlState,
                             <button type="button" className="btn-close" onClick={handleClose}></button>
                         </div>
                         <div className="modal-body">
-                            <p>Are you sure you want to delete this job?</p>
+                            <p>Are you sure you want to request changes?</p>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" onClick={handleClose}>Cancel</button>
-                            <button type="button" className="btn btn-danger text-white">Delete</button>
+                            <button type="button" className="btn btn-danger text-white" onClick={handleRequest}>Request changes</button>
                         </div>
                     </div>
                 </div>
