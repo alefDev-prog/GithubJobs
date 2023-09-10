a13cf1bf1cb5cc5a0f6fa8bd93ef57d1e28e4ed1
alefDev-prog <allan.efendic@icloud.com>
2023-09-10 13:36:05 +0200

    tried to fix cache, unsuccessful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
+import verifyAuth from '@/authMiddleware/auth';
+import { userDataInterface } from '@/interfaces/interface';
 
 const DynamicLogin = dynamic(() =>
   import('./components/login/login').then((mod) => mod.default)
@@ -10,13 +12,33 @@ const DynamicProfile = dynamic(() =>
   import('./components/profile/profile').then((mod) => mod.default)
 );
 
-export default function Home() {
-  
+export default async function Home() {
+
+  const { signal } = new AbortController()
   const nextCookies = cookies();
   const token = nextCookies.get('loggedIn'); 
   
   if(token) {
-      return <DynamicProfile />
+    const uid = await verifyAuth();
+
+    if(typeof uid !== "string") return <DynamicLogin/>
+
+
+    const dataFetch = await fetch(process.env.URL + "/api/getUserData", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(uid),
+      cache: 'no-store',
+      signal
+    },
+    );
+    if(dataFetch.ok){
+      const userData = await dataFetch.json() as userDataInterface;
+      console.log(userData);
+      return <DynamicProfile userData={userData} />
+    }
   }
   else {
     return <DynamicLogin />
